0a20cfdea7ab8d2243abe0c7df161b7235dc549e
amgmacht <68996140+amgmacht@users.noreply.github.com>
2022-07-02 23:05:20 +0200

    some changes
    some changes

---
+++ config.lua
@@ -0,0 +1,24 @@
+-- Here is the page where you can find blips and colors for the blips: https://docs.fivem.net/docs/game-references/blips/
+
+local blip = {
+    --example {title="", colour=, id=, x=, y=, z=},
+    
+    {title="BLIPNAME", colour=1, id=1, size= 1.0, display= 4, x = 222.98, y = -792.31, z = 30.72}, --meetingpoint garage
+  
+  
+}
+  
+Citizen.CreateThread(function()
+
+    for _, amg in pairs(blip) do
+        amg.blip = AddBlipForCoord(amg.x, amg.y, amg.z)
+        SetBlipSprite(amg.blip, amg.id)
+        SetBlipDisplay(amg.blip, amg.display)
+        SetBlipScale(amg.blip, amg.size)
+        SetBlipColour(amg.blip, amg.colour)
+        SetBlipAsShortRange(amg.blip, true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(amg.title)
+        EndTextCommandSetBlipName(amg.blip)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+--
+description 'amg_blip'
+author 'amgmacht - https://discord.gg/KqACGZU5TG'
+version '1.0.0'
+--
+
+client_script {
+    'config.lua'
+}
